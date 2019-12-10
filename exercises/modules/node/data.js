de1666a214aa689b474badb05d1011b05606fa76
Trevor Storey <trevor@photoshelter.com>
2019-12-10 11:53:42 -0700

    [start] exercise 1 done

--- exercises/modules/node/data.js
+++ exercises/modules/node/data.js
@@ -1,4 +1,4 @@
-window.App = {
+module.exports = {
   users: [
     {id: 1, name: 'Weezy'}
   ],
@@ -7,4 +7,4 @@ window.App = {
     {title: 'Me', body: 'Look at my selfie', createdBy: 1},
     {title: 'My doggy', body: 'my dog is better than yours', createdBy: 1}
   ]
-}
+};
